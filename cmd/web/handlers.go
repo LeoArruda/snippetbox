46850d6767f100dcbd3aa8a7e6576d90fdf2a8c7
lvazdear <lvazdear@cmhc-schl.gc.ca>
2025-03-28 17:11:40 -0400

    feat: add db logic

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,48 +1,59 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/LeoArruda/snippetbox/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
 
+	snippets, err := app.snippets.Latest()
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
+	}
+
 	// Initialize a slice containing the paths to the two files. It's important
 	// to note that the file containing our base template must be the *first*
 	// file in the slice.
-	files := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/partials/nav.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
+	// files := []string{
+	// 	"./ui/html/base.tmpl",
+	// 	"./ui/html/partials/nav.tmpl",
+	// 	"./ui/html/pages/home.tmpl",
+	// }
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message, use
-	// the http.Error() function to send an Internal Server Error response to the
-	// user, and then return from the handler so no subsequent code is executed.
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		// Because the home handler is now a method against the application
-		// struct it can access its fields, including the structured logger. We'll
-		// use this to create a log entry at Error level containing the error
-		// message, also including the request method and URI as attributes to
-		// assist with debugging.
-		app.serverError(w, r, err) // Use the serverError() helper.
-		return
-	}
-	// Then we use the Execute() method on the template set to write the
-	// template content as the response body. The last parameter to Execute()
-	// represents any dynamic data that we want to pass in, which for now we'll
-	// leave as nil.
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		// And we also need to update the code here to use the structured logger
-		// too.
-		app.serverError(w, r, err) // Use the serverError() helper.
-	}
+	// // Use the template.ParseFiles() function to read the template file into a
+	// // template set. If there's an error, we log the detailed error message, use
+	// // the http.Error() function to send an Internal Server Error response to the
+	// // user, and then return from the handler so no subsequent code is executed.
+	// ts, err := template.ParseFiles(files...)
+	// if err != nil {
+	// 	// Because the home handler is now a method against the application
+	// 	// struct it can access its fields, including the structured logger. We'll
+	// 	// use this to create a log entry at Error level containing the error
+	// 	// message, also including the request method and URI as attributes to
+	// 	// assist with debugging.
+	// 	app.serverError(w, r, err) // Use the serverError() helper.
+	// 	return
+	// }
+	// // Then we use the Execute() method on the template set to write the
+	// // template content as the response body. The last parameter to Execute()
+	// // represents any dynamic data that we want to pass in, which for now we'll
+	// // leave as nil.
+	// err = ts.ExecuteTemplate(w, "base", nil)
+	// if err != nil {
+	// 	// And we also need to update the code here to use the structured logger
+	// 	// too.
+	// 	app.serverError(w, r, err) // Use the serverError() helper.
+	// }
 
 }
 
@@ -52,7 +63,21 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 		return
 	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+
+	// Use the SnippetModel's Get() method to retrieve the data for a
+	// specific record based on its ID. If no matching record is found,
+	// return a 404 Not Found response.
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%+v", snippet)
+
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
