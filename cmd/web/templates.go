76f98ff27b48e811788989b2b23e0ffcac3db474
lvazdear <lvazdear@cmhc-schl.gc.ca>
2025-03-30 12:12:28 -0400

    feat: add validating form data

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -17,6 +17,7 @@ type templateData struct {
 	CurrentYear int
 	Snippet     models.Snippet
 	Snippets    []models.Snippet
+	Form        any
 }
 
 func (app *application) newTemplateData(r *http.Request) templateData {
