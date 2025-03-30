59675d8ea3c61e84844f6ded2a2be890e01092ec
lvazdear <lvazdear@cmhc-schl.gc.ca>
2025-03-30 10:23:51 -0400

    feat: add recover panic middleware

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -32,6 +33,25 @@ func (app *application) logRequest(next http.Handler) http.Handler {
 
 		app.logger.Info("request", "ip", ip, "proto", proto, "method", method, "uri", uri)
 		// Call the next handler in the chain. This is important! If we don't do this,
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Create a deferred function (which will always be run in the event
+		// of a panic as Go unwinds the stack).
+		defer func() {
+			// Use the builtin recover function to check if there has been a
+			// panic or not. If there has...
+			if err := recover(); err != nil {
+				// Set a "Connection: close" header on the response.
+				w.Header().Set("Connection", "close")
+				// Call the app.serverError() helper method to return 500
+				// Internal Server Error to the user.
+				app.serverError(w, r, fmt.Errorf("%s", err))
+			}
+		}()
 		next.ServeHTTP(w, r)
 	})
 }
